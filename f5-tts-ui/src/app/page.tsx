22f4dc293ed389d7fffb59135b14f1e2d8a2d8cc
Anas Pirzada <102832549+AnasPirzada@users.noreply.github.com>
2025-10-08 16:12:28 +0300

    some changes in ui and apis

---
+++ f5-tts-ui/src/app/page.tsx
@@ -0,0 +1,9 @@
+import TTSInterface from '@/components/TTSInterface';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <TTSInterface />
+    </div>
+  );
+}
